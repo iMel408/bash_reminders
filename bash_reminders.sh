f3f6b96a6c9c94e82e184260c553fcafb003e70a
Melissa <melissascampini@gmail.com>
2019-02-12 00:18:23 -0800

    aliases dont work with $@. using functions instead.

--- bash_reminders.sh
+++ bash_reminders.sh
@@ -1,15 +1,15 @@
 #!/usr/bin/env bash
 
-#alias td='echo $(date "+%Y-%m-%d %H:%M:%S") $* >> /tmp/todo.txt'
-#alias tds='nl /tmp/todo.txt'
-#alias tdd='sed -e '$@d' /tmp/todo.txt'
+#alias td='echo $(date "+%Y-%m-%d %H:%M:%S") $* >> /tmp/todos.txt'
+#alias tds='nl /tmp/todos.txt'
+#alias tdd='sed -e '$@d' /tmp/todos.txt'
 
-# aliases wont work cause $@ gets interpreted when alias is created instead of during the execution.
+# aliases wont work cause $@ gets interpreted when alias is created instead of during execution.
 # will use functions instead
 
 #add to ~/.bashrc file
-function td() { echo $(date "+%Y-%m-%d %H:%M:%S") $@ >> /tmp/todo.txt; }
-function tds() { nl /tmp/todo.txt; }
-function tdd() { sed -e "$@d" /tmp/todo.txt; }
+function td() { echo $(date "+%Y-%m-%d %H:%M:%S") $@ >> /tmp/davestodos.txt; }
+function tds() { nl /tmp/davestodos.txt; }
+function tdd() { sed -e "$@d" /tmp/davestodos.txt; }
 
 typeset -f >> ~/.bashrc

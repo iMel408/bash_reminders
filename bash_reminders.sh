3deb9522f5dd6ce6b9340d0f69bfcb8684cda9a5
Melissa <melissascampini@gmail.com>
2019-02-16 14:25:17 -0800

    added list of bugs that need to be fixed.

--- bash_reminders.sh
+++ bash_reminders.sh
@@ -8,6 +8,8 @@
 # will use functions instead
 
 #add to ~/.bashrc file
-function td() { echo $(date "+%Y-%m-%d %H:%M:%S") $@ >> /tmp/davestodo.txt; }
+function td() { echo $(date "+%Y-%m-%d %H:%M:%S") '$@' >> /tmp/davestodo.txt; }
+    # TODO: escape aphostrophe and exclamation points
 function tds() { nl /tmp/davestodo.txt; }
 function tdd() { sed -i.bak -e "${1}d" /tmp/davestodo.txt; }
+    #TODO: fix empty arg deleting entire list
